15fbb99b9fa347f189d862376db6f774f208bde1
whynot <6sidodji6@gmail.com>
2025-01-23 19:34:34 +0200

    Process

---
+++ Assets/ColorData.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class ColorData
+{
+    public int x;
+    public int y;
+    public float r;
+    public float g;
+    public float b;
+    public float a;
+
+    public ColorData(int x, int y, Color color)
+    {
+        this.x = x;
+        this.y = y;
+        this.r = color.r;
+        this.g = color.g;
+        this.b = color.b;
+        this.a = color.a;
+    }
+}
